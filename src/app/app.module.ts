524da43cded0409e03aba9f9780ac0452caaa264
shiveshjha <shivesh.jha@hotmail.com>
2018-11-28 12:53:14 +0530

    test component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestcomponentComponent
   ],
   imports: [
     BrowserModule
