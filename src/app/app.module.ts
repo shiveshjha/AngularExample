7506e8a19c8a28750b5f5482fe150004f333ae94
shiveshjha <shivesh.jha@hotmail.com>
2018-11-29 12:13:24 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestcomponentComponent,
-    HeaderComponentComponent
+    HeaderComponentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
